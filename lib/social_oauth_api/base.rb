df93b895aa7de4b04060b95daa2d2b473bfdf7d7
Spirit <neverlandxy.naix@gmail.com>
2014-04-11 10:53:08 +0800

    增加timeout

--- lib/social_oauth_api/base.rb
+++ lib/social_oauth_api/base.rb
@@ -4,6 +4,8 @@ module SocialOauthApi
   class Base
     attr_accessor :client_id, :client_secret, :access_token
 
+    HTTP_TIMEOUT = 15
+
     def initialize options
       @client_id     = options[:client_id]
       @client_secret = options[:client_secret]
@@ -11,13 +13,15 @@ module SocialOauthApi
     end
 
     def get url, query
-      uri = URI(url)
-      uri.query = URI.encode_www_form(query)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if uri.scheme == 'https'
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      response.body
+      timeout(HTTP_TIMEOUT) do
+        uri = URI(url)
+        uri.query = URI.encode_www_form(query)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+        response.body
+      end
     end
   end
 end
